31a34c9b71c9bd19b0c282826f2b16e44c75615f
osarsari <o.sarsari@gmail.com>
2024-02-14 15:34:25 +0100

    simplified fragtrap

--- ex02/FragTrap.cpp
+++ ex02/FragTrap.cpp
@@ -6,26 +6,20 @@
 /*   By: osarsari <osarsari@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/20 14:48:11 by osarsari          #+#    #+#             */
-/*   Updated: 2023/12/20 14:50:26 by osarsari         ###   ########.fr       */
+/*   Updated: 2024/02/14 15:33:56 by osarsari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "FragTrap.hpp"
 
-FragTrap::FragTrap(void) : ClapTrap()
+FragTrap::FragTrap(void) : ClapTrap("default", 100, 100, 30)
 {
 	std::cout << "FragTrap default constructor called" << std::endl;
-	this->_hitPoints = 100;
-	this->_energyPoints = 100;
-	this->_attackDamage = 30;
 }
 
-FragTrap::FragTrap(std::string name) : ClapTrap(name)
+FragTrap::FragTrap(std::string name) : ClapTrap(name, 100, 100, 30)
 {
 	std::cout << "FragTrap parameter constructor called" << std::endl;
-	this->_hitPoints = 100;
-	this->_energyPoints = 100;
-	this->_attackDamage = 30;
 }
 
 FragTrap::FragTrap(FragTrap const &src) : ClapTrap(src)
@@ -48,5 +42,6 @@ FragTrap &FragTrap::operator=(FragTrap const &rhs)
 
 void FragTrap::highFivesGuys(void)
 {
-	std::cout << "FragTrap " << this->_name << " wants to high five you!" << std::endl;
+	std::cout << "FragTrap " << this->_name << " wants to high five you!"
+			  << std::endl;
 }
